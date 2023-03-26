0d57775d0f366f679b8ee35b03310b76e0e02bf7
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-03-26 17:19:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,173 @@
-// Instanciation de l'objet UAParser
-var parser = new UAParser();
+(function() {
+  'use strict';
 
-// Appel des méthodes de l'objet pour détecter le type d'appareil
-var resultat = "";
-var resultat_device = parser.getDevice();
-var resultat_os = parser.getOS();
-var resultat_browser = parser.getBrowser();
+  var module = {
+    options: [],
+    header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+    dataos: [{
+        name: 'Windows Phone',
+        value: 'Windows Phone',
+        version: 'OS'
+      },
+      {
+        name: 'Windows',
+        value: 'Win',
+        version: 'NT'
+      },
+      {
+        name: 'iPhone',
+        value: 'iPhone',
+        version: 'OS'
+      },
+      {
+        name: 'iPad',
+        value: 'iPad',
+        version: 'OS'
+      },
+      {
+        name: 'Kindle',
+        value: 'Silk',
+        version: 'Silk'
+      },
+      {
+        name: 'Android',
+        value: 'Android',
+        version: 'Android'
+      },
+      {
+        name: 'PlayBook',
+        value: 'PlayBook',
+        version: 'OS'
+      },
+      {
+        name: 'BlackBerry',
+        value: 'BlackBerry',
+        version: '/'
+      },
+      {
+        name: 'Macintosh',
+        value: 'Mac',
+        version: 'OS X'
+      },
+      {
+        name: 'Linux',
+        value: 'Linux',
+        version: 'rv'
+      },
+      {
+        name: 'Palm',
+        value: 'Palm',
+        version: 'PalmOS'
+      }
+    ],
+    databrowser: [{
+        name: 'Chrome',
+        value: 'Chrome',
+        version: 'Chrome'
+      },
+      {
+        name: 'Firefox',
+        value: 'Firefox',
+        version: 'Firefox'
+      },
+      {
+        name: 'Safari',
+        value: 'Safari',
+        version: 'Version'
+      },
+      {
+        name: 'Internet Explorer',
+        value: 'MSIE',
+        version: 'MSIE'
+      },
+      {
+        name: 'Opera',
+        value: 'Opera',
+        version: 'Opera'
+      },
+      {
+        name: 'BlackBerry',
+        value: 'CLDC',
+        version: 'CLDC'
+      },
+      {
+        name: 'Mozilla',
+        value: 'Mozilla',
+        version: 'Mozilla'
+      }
+    ],
+    init: function() {
+      var agent = this.header.join(' '),
+        os = this.matchItem(agent, this.dataos),
+        browser = this.matchItem(agent, this.databrowser);
 
-// Construction de la chaîne de résultat
-resultat += "Appareil : " + resultat_device.vendor + " " + resultat_device.model + "<br>";
-resultat += "Système d'exploitation : " + resultat_os.name + " " + resultat_os.version + "<br>";
-resultat += "Navigateur : " + resultat_browser.name + " " + resultat_browser.version;
+      return {
+        os: os,
+        browser: browser
+      };
+    },
+    matchItem: function(string, data) {
+      var i = 0,
+        j = 0,
+        html = '',
+        regex,
+        regexv,
+        match,
+        matches,
+        version;
 
-// Affichage des résultats dans l'élément HTML approprié
-document.getElementById("resultat").innerHTML = resultat;
+      for (i = 0; i < data.length; i += 1) {
+        regex = new RegExp(data[i].value, 'i');
+        match = regex.test(string);
+        if (match) {
+          regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+          matches = string.match(regexv);
+          version = '';
+          if (matches) {
+            if (matches[1]) {
+              matches = matches[1];
+            }
+          }
+          if (matches) {
+            matches = matches.split(/[._]+/);
+            for (j = 0; j < matches.length; j += 1) {
+              if (j === 0) {
+                version += matches[j] + '.';
+              } else {
+                version += matches[j];
+              }
+            }
+          } else {
+            version = '0';
+          }
+          return {
+            name: data[i].name,
+            version: parseFloat(version)
+          };
+        }
+      }
+      return {
+        name: 'unknown',
+        version: 0
+      };
+    }
+  };
+
+  var e = module.init(),
+    debug = '';
+
+  debug += 'os.name = ' + e.os.name + '<br/>';
+  debug += 'os.version = ' + e.os.version + '<br/>';
+  debug += 'browser.name = ' + e.browser.name + '<br/>';
+  debug += 'browser.version = ' + e.browser.version + '<br/>';
+
+  debug += '<br/>';
+  debug += 'navigator.userAgent = ' + navigator.userAgent + '<br/>';
+  debug += 'navigator.appVersion = ' + navigator.appVersion + '<br/>';
+  debug += 'navigator.platform = ' + navigator.platform + '<br/>';
+  debug += 'navigator.vendor = ' + navigator.vendor + '<br/>';
+
+  document.getElementById('log').innerHTML = debug;
+}());
+  document.getElementById('log').innerHTML = debug;
+}());
